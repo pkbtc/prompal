02ea99cba91f80cc5068341df43428763ff1e12b
pkbtc <parasarkashyap06@gmail.com>
2025-04-20 00:49:52 +0530

    .

--- src/data/vipUsersData.js
+++ src/data/vipUsersData.js
@@ -5,13 +5,12 @@ export const vipUsers = [
     name: 'Karyikey Singh',
     bio: "Hey there! I'm Karyikey, a senior at Riverside High. I love photography, playing guitar, and exploring new hiking trails. Looking for someone who shares my passion for adventure and music. My ideal prom night would include great music, dancing, and creating memories that last a lifetime!",
     images: [
-      '/images/vip/karyikey-1.jpg',
-      '/images/vip/karyikey-2.jpg',
-      '/images/vip/karyikey-3.jpg',
-      '/images/vip/karyikey-4.jpg',
-      '/images/vip/karyikey-5.jpg'
+      'https://firebasestorage.googleapis.com/v0/b/ai-prowered-sih.appspot.com/o/WhatsApp%20Image%202025-04-20%20at%2000.32.09_2a8bea56.jpg?alt=media&token=384bbe59-fc84-483d-abe3-1b25ba7e3c2c',
+      'https://firebasestorage.googleapis.com/v0/b/ai-prowered-sih.appspot.com/o/WhatsApp%20Image%202025-04-20%20at%2000.32.10_1f3d4a6b.jpg?alt=media&token=3436373e-1d8c-42ad-b7de-1f3bafc3ab0f',
+      'https://firebasestorage.googleapis.com/v0/b/ai-prowered-sih.appspot.com/o/WhatsApp%20Image%202025-04-20%20at%2000.32.10_c830859e.jpg?alt=media&token=ffff1b9d-8ebb-4c84-a6ec-2dc9f1db9030',
+      'https://firebasestorage.googleapis.com/v0/b/ai-prowered-sih.appspot.com/o/WhatsApp%20Image%202025-04-20%20at%2000.32.11_b157c4bd.jpg?alt=media&token=1a33e6d7-a33a-45cd-8c62-feaaa8b0f885'
     ],
-    profileImage: '/images/vip/karyikey-profile.jpg',
+    profileImage: 'https://firebasestorage.googleapis.com/v0/b/ai-prowered-sih.appspot.com/o/WhatsApp%20Image%202025-04-20%20at%2000.32.09_2a8bea56.jpg?alt=media&token=384bbe59-fc84-483d-abe3-1b25ba7e3c2c',
     vibeType: 'PARTY_ENERGETIC'
   },
   {
